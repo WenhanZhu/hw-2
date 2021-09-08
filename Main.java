1fc2a0e6f1f5dfc11f7de29e3fdbeab928793876
MickZ <89829528+WenhanZhu@users.noreply.github.com>
2021-09-08 02:10:20 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("My favourite cocki is fortune coocki");
+  double weight;
+  double calories;
+  String color;
+  int count;
+
+
+  }
+}
